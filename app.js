fabd706662a096d075975a13faace4ab7279d2d4
ogbon <onethinneedful@gmail.com>
2022-10-31 16:47:10 +0100

    change slackUsername

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 
 app.use((req,res) => {
    res.status(200).send({
-     slackUsername: 'segun amosu',
+     slackUsername: 'segun-amosu',
      backend: true,
      age: 40,
      bio: 'I am a NodeJS backend developer'
