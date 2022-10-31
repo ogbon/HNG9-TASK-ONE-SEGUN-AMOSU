2113f6fe783c2a25385bfbb66c8b838a92f85752
ogbon <onethinneedful@gmail.com>
2022-10-31 14:52:46 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use((req,res) => {
+   res.status(200).send({
+     slackUsername: 'segun amosu',
+     backend: true,
+     age: 40,
+     bio: 'I am a NodeJS backend developer'
+   })
+})
+
+app.listen(PORT)
